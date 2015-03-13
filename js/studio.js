17895437d5a3bfd73ecb714727ec1e6c1e0ec604
seblerom <soloban88@gmail.com>
2015-03-13 13:04:22 -0300

    Studio section

---
+++ js/studio.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$("#studio1 a").hover(function(){
+		$("#spin1").css("background-color","red");
+	});
+
+	$("#studio2 a").hover(function(){
+		$("#spin2").css("background-color","black");
+	});
+
+	$("#studio3 a").hover(function(){
+		$("#spin3").css("background-color","orange");
+	});
+
+	$("#studio4 a").hover(function(){
+		$("#spin4").css("background-color","green");
+	});	
+});
+
+

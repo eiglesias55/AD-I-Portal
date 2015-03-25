d649a68b29fb17902d97e9d91dca6c3e41307b35
Matias <eiglesias@grupoassa.com>
2015-03-25 18:04:18 -0300

    Home page

--- js/adi-in-numbers.js
+++ js/adi-in-numbers.js
@@ -1,23 +1,6 @@
 $(document).ready(function(){
 	$(".adi-number-box").stop().hover(function(){
-		$(".adi-number-number").animate({
-    			marginLeft:"10",
-    			marginRight:"-10"
-  				}, 100,function(){
-  					$(".adi-number-number").animate({
-    					marginLeft:"-10",
-    					marginRight:"10"
-  				}, 100);
-  				});
-  		$(".adi-number-text").animate({
-				marginLeft:"-10",
-				marginRight:"10"
-				}, 100,function(){
-					$(".adi-number-text").animate({
-						marginLeft:"10",
-						marginRight:"-10"
-						}, 100);
-				});
+	
 		
 	});
 });
--- js/studio.js
+++ js/studio.js
@@ -3,24 +3,31 @@ $(document).ready(function(){
 	var studiosIds = ["studio1","studio2","studio3","studio4","studio5"];
 	var contentsIds = ["studio-content1","studio-content2","studio-content3","studio-content4","studio-content5"];
 	var studioClicked = false;
+	var studioHover = false;
 
 	for(i = 0 ; i < studiosIds.length ; i++){
 		$("#"+studiosIds[i]).hover(function(){
-			var dataTrigger = $(this).data("trigger");
-			$("#"+studiosIds[i]).css("border","2px solid black");
+			studioHover = true;
 			$(".adi-studiosbox-content > * ").stop().hide(0);
 			var dataTrigger = $(this).data("trigger");
 			$("#" + dataTrigger).stop().show(0);
-			});
-		$("#"+studiosIds[i]).click(function(){
-			studioClicked = true;
-			$("#"+studiosIds[i]).css("border","2px solid black");
-			var dataTrigger = $(this).data("trigger");
-			$(".adi-studiosbox-content").jQuery(":visible").stop().fadeOut(0);
-			var dataTrigger = $(this).data("trigger");
-			$("#" + dataTrigger).stop().fadeIn(300);
-			});
+			}, function(){ studioHover = false});
+			$("#"+studiosIds[i]).click(function(){
+				studioClicked = true;
+				var actual = 0;
+				while($("#"+studiosIds[actual]).attr("class") != "adi-studiosbox-box-active"){
+							actual++;
+					}	
+				$("#"+studiosIds[actual]).removeClass();
+				$("#"+studiosIds[actual]).addClass("adi-studiosbox-box");
+				$(this).removeClass();
+				$(this).addClass("adi-studiosbox-box-active");
+				var dataTrigger = $(this).data("trigger");
+				$("#" + dataTrigger).stop().fadeIn(300);
+				});
 	}
+
+	/* ------------------------------------------------ hover ------------------------------------------------------ */
 
 
 	$(".adi-studiosbox-box").hover(function(){
@@ -28,25 +35,29 @@ $(document).ready(function(){
 	},function(){$(this).stop().fadeTo(200,1);
 	});
 
-	$("#studio4").click(function(){
-		$("#studio4 a img").attr("src","Img/digital-content-clicked.png");
-	});
+	/* ----------------------------------------------- Studio autochange ------------------------------------------ */
 
-		/*window.setInterval(function(){
-			if (studioClicked == false){
+		window.setInterval(function(){
+			if (studioClicked == false && studioHover == false){
 				var i=0;
-				while($("#"+contentsIds[i]).css("display") != "block"){
+				while($("#"+studiosIds[i]).attr("class") != "adi-studiosbox-box-active"){
 						i++;
 				}
+				$("#"+studiosIds[i]).removeClass();
+				$("#"+studiosIds[i]).addClass("adi-studiosbox-box");
 				$("#"+contentsIds[i]).stop().fadeOut(0);
 				if(i == contentsIds.length - 1){
 					i=-1;
 				}
-				$("#"+contentsIds[i+1]).stop().fadeIn(0);	
+				$("#"+contentsIds[i+1]).stop().fadeIn(0);
+				$("#"+studiosIds[i+1]).removeClass();
+				$("#"+studiosIds[i+1]).addClass("adi-studiosbox-box-active");	
 			}else{
 				studioClicked = false;	
 			}
-		},4000);*/
+		},4000);
+
+	/* ------------------------------------------------------------------------------------------------------------- */
 });
 
 

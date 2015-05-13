707cbbe5e12a13af7f90afa345acf42ba85c5da5
unknown <eiglesias@ASSA66.grupoassa>
2015-05-13 05:48:22 -0700

    Juli Changes

--- js/our-team.js
+++ js/our-team.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
   $(".adi-scroll-button img").click(function(){
     $(this).hide(0);
     if($("p#paragraph2").is(":hidden")){
-      $("#paragraph2,p#paragraph3").show(1000);
+      $("#paragraph2,p#paragraph3").slideDown(1000);
       $(".adi-scroll-button img").attr("src","Img/upButton.png");
       $(this).show(1000);
     }else{
-      $("#paragraph2,p#paragraph3").hide(1000);
+      $("#paragraph2,p#paragraph3").slideUp(1000);
       $(".adi-scroll-button img").attr("src","Img/downButton.png");
       $(this).show(1000); 
     }

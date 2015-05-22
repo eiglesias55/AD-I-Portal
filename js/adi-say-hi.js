5853331cf7b9a199dc4a82d7188cac244fd2f02f
Julia Morelli <jmorelli@grupoassa.com>
2015-05-22 12:47:03 -0700

    Actualización de navbars

--- js/adi-say-hi.js
+++ js/adi-say-hi.js
@@ -4,3 +4,8 @@ $(document).ready(function () {
 	$("#adi-say-hi").height(sectionWidth/1.87);
 	
 });
+
+$(window).resize(function () {
+		var sectionWidth = $("#adi-say-hi").width();
+		$("#adi-say-hi").height(sectionWidth/1.87);
+});

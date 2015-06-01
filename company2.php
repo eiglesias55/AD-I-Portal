9f2acc649f210f4bf7bad83fef8d509bb0053903
Julia Morelli <jmorelli@grupoassa.com>
2015-06-01 05:52:39 -0700

    Pruebas de PHP

---
+++ company2.php
@@ -0,0 +1,234 @@
+<?php include("includes/header.php");?>
+	<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+
+
+	<div class="adi-section" id="we-are-adi-section">
+		<a href="#" name="we-are-adi-section"></a>
+		<div id="bgVideo">
+			<video autoplay="autoplay" width="100%" height="auto" poster="ccs/images/company.png" loop="loop">
+				<source type="video/mp4" src="Img/VideoDummy.mp4" displaymode="Original"></source>
+			</video>
+		</div>
+		<div id="content">
+			<h1 id="section-title"><span>A</span>D&I</h1>
+			<h2 id="section-subtitle">We are motivated by the challenge of generating value added to our clients and to society, encouraging technological innovation and people empowerment.</h2>
+			<a href="#our-history-section"><img src="img/downButton-white.png"></a>
+		</div>
+	</div>
+   
+   
+   <!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+    <div class="adi-section" id="our-history-section">
+		<a href="#" name="our-history-section"></a>
+  		<h1 id="section-title">Our History</h1>
+		<div id="timeline">
+			<img src="http://d585tldpucybw.cloudfront.net/sfimages/default-source/company/timeline/timeline-01.png?sfvrsn=2" id="img1">
+		</div>
+   </div>
+   
+   
+   
+<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+   <div class="adi-section" id="our-team-section">
+		<a href="#" name="our-team-section"></a>
+  		<h3 id="section-title">Our Team</h3>
+      <p id="section-subtitle">We are gA's developers.</p>
+      <div class="paragraph-board">
+        <p id="paragraph1">To be innovative developers, with forward thinking and constant commitment, is in our foundations.</p>
+        <p id="paragraph2">We achieve this by training professionals in different technologies, whom interact in a full of opportunities environment and in constant challenge.</p>
+        <p id="paragraph3">Sharing same culture and self-identity, encouraging personal and professional grow of each team member.</p>
+      </div>
+   </div>
+
+
+<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+    <div class="adi-section" id="adiinnumbers-company">
+		<a href="#" name="adi-in-numbers-section"></a>
+        <h1 style="text-align:center; margin:10px 0;" id="section-title">AD&I in numbers</h1>
+        <div class="adi-number-box" id="offices-number">
+          <div class="adi-number-number">10</div>
+          <div class="adi-number-text">Offices</div>
+        </div>
+        <div class="adi-number-box" id="contribuitors-number">
+          <div class="adi-number-number">400+</div>
+          <div class="adi-number-text">Developers</div>
+        </div>
+        <div class="adi-number-box" id="development-hours-number">
+          <div class="adi-number-number">5M+</div>
+          <div class="adi-number-text">Dev & PM hours</div>
+        </div>
+    </div>
+
+<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+    <div class="adi-section" id="testimonials-company">
+		<a href="#" name="testimonials-section"></a>
+		<h1 style="text-align:center" id="section-title">Customer Testimonials</h1>
+      
+		<div class="adi-testimonials">
+			<div class="adi-testimonials-text" id="adi-testimonial-1">
+				Success in the implementation of this new project was largely due to the fact that we formed a synergic and synchronized work group with everyone involved, which has enabled Wal-Mart to optimize the non-commercial annual procurement controls thanks to extremely detailed information.
+				<span>— Fernando Di Silvestro, Business Analyst, Wal-Mart Argentina</span>
+			</div>
+		</div>
+
+      <div class="adi-testimonials">
+        <div class="adi-testimonials-text" id="adi-testimonial-2">
+            The team that started the project is the same one that concluded the project. For us, this is very important. I would also highlight Grupo ASSA’s service and capacity to respond during and after the project.
+            <span>— Alberto Chiari, Technical and Geographic Systems Manager for Agua y Saneamientos Argentinos S.A. (AySA)</span>
+        </div>
+      </div>
+
+      <div class="adi-testimonials">
+        <div class="adi-testimonials-text" id="adi-testimonial-3">
+            One must highlight the fact that the project was completed on schedule and executed within the original budget, without compromising the quality of the product received.
+            <span>— Nicolás Pedroza IT Manager for Pilkington Automotive Argentina</span>
+        </div>
+      </div>
+      
+   </div>
+    
+	
+	<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+    <div class="adi-section" id="adi-from-inside">
+		<a href="#" name="ga-life-section"></a>
+		<h1 style="text-align:center" id="section-title">gA Life</h1>
+		<div id="ga-life">
+			<img src="Img/gALife-compose-1.png" id="ga-life-1">
+			<img src="Img/gALife-compose-2.png" id="ga-life-2">
+			<img src="Img/gALife-compose-3.png" id="ga-life-3">
+		</div>
+		<div id="ga-life-text">
+			This is some fancy text about how much of an awesome place AD&I is and why our devs are so effin happy.
+		</div>
+		<a class="learnMore" href="#">Join us!</a>
+	</div>
+	
+	<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+    <div class="adi-section" id="adi-say-hi" style="background-image: url('Img/adi-say-hi.png')">
+		<a href="#" name="say-hi-section"></a>
+  		<h1 id="section-title">Say hi!</h1>
+		
+		<div class="say-hi-box" id="offices-hi-number">
+          <div class="say-hi-number">##</div>
+          <div class="say-hi-text">Offices</div>
+        </div>
+        <div class="say-hi-box" id="countries-hi-number">
+          <div class="say-hi-number">##</div>
+          <div class="say-hi-text">Countries</div>
+        </div>
+        <div class="say-hi-box" id="continents-hi-number">
+          <div class="say-hi-number">#</div>
+          <div class="say-hi-text">Continents</div>
+        </div>
+		<br>
+		<a href="#" class="learnMore">Contact Us</a>
+   </div>
+   
+   <!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Seccion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    <div class="adi-section" id="">
+  		<h1 style="text-align:center"></h1>
+  		
+   </div>
+   
+<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Footer %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+
+  <footer>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> AD&I </h3>
+                    <ul>
+                        <li> <a href="#"> Company </a> </li>
+                        <li> <a href="#">Our Studios </a> </li>
+                        <li> <a href="#"> gA University </a> </li>
+						<li> <a href="#"> Methodology </a> </li>
+                    </ul>
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Company </h3>
+                    <ul>
+                        <li> <a href="#we-are-adi-section"> We are AD&I </a> </li>
+                        <li> <a href="#our-history-section"> Our History </a> </li>
+						<li> <a href="#our-team-section"> Our Team </a> </li>
+                        <li> <a href="#adi-in-numbers-section"> AD&I in numbers </a> </li>
+                        <li> <a href="#testimonials-section"> Customer Testimonials </a> </li>
+                        <li> <a href="#ga-life-section"> gA Life </a> </li>
+                        <li> <a href="#say-hi-section"> Say hi! </a> </li>
+                    </ul>
+                </div>
+                
+				<div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Studios </h3>
+                    <ul>
+                        <li> <a href="#"> UX & Social </a> </li>
+                        <li> <a href="#"> Mobile </a> </li>
+						<li> <a href="#"> e-Commerce </a> </li>
+                        <li> <a href="#"> Digital Content </a> </li>
+                        <li> <a href="#"> ERP </a> </li>
+                    </ul>
+                </div>
+				
+				<div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> gA University </h3>
+                    
+                </div>
+                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Methodology </h3>
+                    
+                </div>
+                <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
+                    <h3> Our headquarters </h3>
+                    <ul>
+                        <li>
+                            <div class="adi-our-headquarters-img">
+                              <img src="Img/where-to-find-us.png">
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="social">
+                       <li> <a href="https://www.facebook.com/grupoassa"> <img src="Img/facebook-icon.png"> </a> </li>
+                        <li> <a href="https://www.youtube.com/user/grupoassa"> <img src="Img/youtube-icon.png"></a> </li>
+                        <li> <a href="https://twitter.com/grupo_assa"> <img src="Img/twitter-icon.png"> </a> </li>
+                        <li> <a href="https://www.linkedin.com/company/grupo-assa"> <img src="Img/linkedin-icon.png"> </a> </li>
+                        <li> <a href="https://plus.google.com/115745625107023367096/posts"> <img src="Img/googleplus-icon.png"> </a> </li>
+                    </ul>
+                </div>
+            </div>
+            <!--/.row--> 
+        </div>
+        <!--/.container--> 
+    </div>
+    <!--/.footer-->
+    
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="pull-left"> Copyright © Grupo Assa. All right reserved. </p>
+            <div class="pull-right">
+                <ul class="nav nav-pills payments">
+                  <li><i class="fa fa-cc-visa"></i></li>
+                    <li><i class="fa fa-cc-mastercard"></i></li>
+                    <li><i class="fa fa-cc-amex"></i></li>
+                    <li><i class="fa fa-cc-paypal"></i></li>
+                </ul> 
+            </div>
+        </div>
+    </div>
+    <!--/.footer-bottom--> 
+</footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/navbar.js"></script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Company</title>
+
+    <!-- Favicon -->
+    <link href="img/AD&I-favicon.png" rel="icon">
+
+
+    <!-- Bootstrap -->
+  
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom CSS-->
+   <link type="text/css" rel="stylesheet" href="css/common.css">
+    <link type="text/css" rel="stylesheet" href="css/navbar.css">
+	<link type="text/css" rel="stylesheet" href="css/we-are-adi.css">
+	<link type="text/css" rel="stylesheet" href="css/our-history.css">
+	<link type="text/css" rel="stylesheet" href="css/our-team.css">
+	<link type="text/css" rel="stylesheet" href="css/adi-in-numbers-company.css">
+	<link type="text/css" rel="stylesheet" href="css/testimonials.css">
+	<link type="text/css" rel="stylesheet" href="css/adi-from-inside.css">
+	<link type="text/css" rel="stylesheet" href="css/adi-say-hi.css">
+    <link type="text/css" rel="stylesheet" href="css/company.css">
+    
+	
+
+    <!-- Custom fonts-->
+    <link href="http://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" type="text/css">   
+    <link href="http://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet" type="text/css">
+    
+    <!-- Custom JavaScript-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="js/common.js"></script>
+	<script src="js/adi-from-inside.js"></script>
+	<script src="js/we-are-adi.js"></script>
+	<script src="js/testimonials.js"></script>
+	<script src="js/adi-say-hi.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+<!--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comienzo de Navbar %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-->
+    
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+          <img src="Img/AD&I - Portal.png">
+          
+        </a>
+		
+		<a href="#" id="hiring">We're hiring!</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="company.html">Company<span class="sr-only">(current)</span></a></li>
+		  <li><a href="#">Studios</a></li>
+          <li><a href="#">gA University</a></li>
+          <li><a href="#">Methodology</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>

ea7ab1f2c588f0431d9bb5bc30239a0e14847676
unknown <eiglesias@ASSA66.grupoassa>
2015-06-01 13:21:13 -0700

    Navbar - actualización de links

--- includes/header.php
+++ includes/header.php
@@ -56,7 +56,7 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li><a href="company.php">Company<span class="sr-only">(current)</span></a></li>
+          <li><a href="company">Company<span class="sr-only">(current)</span></a></li>
 		  <li><a href="#">Studios</a></li>
           <li><a href="#">gA University</a></li>
           <li><a href="#">Methodology</a></li>
